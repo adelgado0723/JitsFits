8093e73118ef19b54a0e29536a6244b1fee32cd7
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-08-27 17:32:32 -0400

    Completed module 2 - Styled Components

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -4,7 +4,8 @@ const Meta = () => (
   <Head>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+    {/* <meta httpEquiv="X-UA-Compatible" content="ie=edge" /> */}
+    <meta httpEquiv="content-type" content="text/html" />
     <link rel="shortcut icon" href="/static/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <title>Sick Fits!</title>
