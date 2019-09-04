cbb0c47af7fb6ce6581bc818b15b74afad4958f0
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-09-04 17:14:01 -0400

    at 9:57 of lesson 14

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,24 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    // create a dog
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+  // removeDog(parent, args, ctx, info) {
+  //   if (global.dogs) {
+  //     let removedDogs = [];
+  //     global.dogs.forEach((dog, index) => {
+  //       if (dog.name === args.name) {
+  //         removedDogs.push(global.dogs.splice(index, 1));
+  //       }
+  //     });
+  //     return removedDogs;
+  //   }
+  // }
+  createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
   }
 };
 
