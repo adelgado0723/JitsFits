9d49a79f97d26f209399ee9421ad10c70845ee24
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-09-10 16:32:33 -0400

    Completed video 14. Yoga and Prisma are now set up

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,24 +1,17 @@
 const Mutations = {
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   // create a dog
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
-  // removeDog(parent, args, ctx, info) {
-  //   if (global.dogs) {
-  //     let removedDogs = [];
-  //     global.dogs.forEach((dog, index) => {
-  //       if (dog.name === args.name) {
-  //         removedDogs.push(global.dogs.splice(index, 1));
-  //       }
-  //     });
-  //     return removedDogs;
-  //   }
-  // }
-  createItem(parent, args, ctx, info) {
+  async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
+
+    // createItem returns a promise
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
   }
 };
 
