11bcbd30395ad6a9ab1f34acbfa1a435e2bf5fb4
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-10-15 13:16:12 -0400

    Completed video 24 - signup user on backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
@@ -13,6 +16,7 @@ const Mutations = {
     );
     return item;
   },
+
   updateItem(parent, args, ctx, info) {
     // First, take a copu of the updates
     const updates = { ...args };
@@ -30,6 +34,7 @@ const Mutations = {
       info
     );
   },
+
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // 1. Find the item
@@ -41,6 +46,40 @@ const Mutations = {
 
     // 3. Delete the item
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    // Make their email lowercase
+    args.email = args.email.toLowerCase();
+
+    // Hash their password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // Create user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ["USER"] }
+        }
+      },
+      info
+    );
+
+    // Signing in the user with the credentials they just created
+
+    // Create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // Set JWT as a cookie on the response
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // Set to las for 1 year
+    });
+
+    // Return user to the browser
+    return user;
   }
 };
 
