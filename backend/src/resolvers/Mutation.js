c5148e8ea24d4e9049467e4503609a502f560aa3
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-10-18 17:00:54 -0400

    Completed video 27 - Sign in form and custom error handling

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const ONE_YEAR_COOKIE = 1000 * 60 * 60 * 24 * 365;
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if they are logged in
@@ -75,10 +77,36 @@ const Mutations = {
     // Set JWT as a cookie on the response
     ctx.response.cookie("token", token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365 // Set to las for 1 year
+      maxAge: ONE_YEAR_COOKIE // Set to las for 1 year
     });
 
     // Return user to the browser
+    return user;
+  },
+
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. Check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+
+    // 2. Check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Password!");
+    }
+
+    // 3. Generate the JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    // 4. Set the cookie with the token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: ONE_YEAR_COOKIE
+    });
+
+    // 5. Return the user
     return user;
   }
 };
