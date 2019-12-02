fd716f10cd4886915cd0774eb5f9ac8e34e4d089
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-12-02 17:16:42 -0500

    At 9:08 of video 49

---
+++ frontend/components/TakeMyMoney.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { Mutation } from 'react-apollo';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import Error from './ErrorMessage';
+import User, { CURRENT_USER_QUERY } from './User';
+
+function totalItems(cart) {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+}
+
+class TakeMyMoney extends React.Component {
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="Jits Fits"
+            description={`Order of ${totalItems} items!`}
+          >
+            {this.props.children}
+          </StripeCheckout>
+        )}
+      </User>
+    );
+  }
+}
+
+export default TakeMyMoney;
