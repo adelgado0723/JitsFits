92f6d9e9b6b24275df9cfe639b869d5c723e1c59
Andres Rafael Delgado <adelgado0723@gmail.com>
2019-12-16 17:28:12 -0500

    Geting errors at 08:48 of video 59

---
+++ frontend/__tests__/SingleItem.test.js
@@ -0,0 +1,31 @@
+import { shallow, mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import SingleItem, { SINGLE_ITEM_QUERY } from '../components/SingleItem';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { fakeItem } from '../lib/testUtils';
+
+describe('<SingleItem />', () => {
+  it('renders with proper data', async () => {
+    const mocks = [
+      {
+        request: { query: SINGLE_ITEM_QUERY, variables: { id: '123' } },
+        result: {
+          data: {
+            item: fakeItem(),
+          },
+        },
+      },
+    ];
+    const wrapper = mount(
+      <MockedProvider mocks={mocks}>
+        <SingleItem id="123" />
+      </MockedProvider>
+    );
+    // console.log(wrapper.text());
+    expect(wrapper.text()).toContain('Loading...');
+    await wait();
+    wrapper.update();
+    // console.log(wrapper.debug());
+  });
+});
